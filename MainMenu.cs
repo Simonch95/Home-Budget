cad69f99137acae843116a9ba8432402bb76f4f3
Simonch95 <55145117+Simonch95@users.noreply.github.com>
2020-07-25 18:51:59 +0200

    first commit

---
+++ MainMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace CapturePicture
+{
+    [Activity(Label = "MainMenu", MainLauncher = true)]
+    public class MainMenu : Activity
+    {
+        public Button addButton;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.main_menu);
+            // Create your application here
+            addButton = FindViewById<Button>(Resource.Id.AddReceiptBtn);
+            addButton.Click += AddButton_Click;
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+           StartActivity(typeof(AddReceipt));
+        }
+    }
+}
---
+++ obj/Debug/90/android/typemap.jm.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 09afb4cd7c760c2801415b474cfb19ea3ab2836c */
+	.file	"typemap.jm.inc"
+
+	/* Mapping header */
+	.section	.data.jm_typemap,"aw",@progbits
+	.type	jm_typemap_header, @object
+	.p2align	2
+	.global	jm_typemap_header
+jm_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1569
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	117
+	.size	jm_typemap_header, 16
+
+	/* Mapping data */
+	.type	jm_typemap, @object
+	.global	jm_typemap
+jm_typemap:
+	.size	jm_typemap, 411079
+	.include	"typemap.jm.inc"
